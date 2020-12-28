60802f955637962e54ca603d2ad1d5271f8396fc
derek <derek_mcp@163.com>
2020-12-28 18:04:46 +0800

    三级分类

--- product-server/src/main/java/com/derek/mall/product/controller/CategoryController.java
+++ product-server/src/main/java/com/derek/mall/product/controller/CategoryController.java
@@ -1,6 +1,7 @@
 package com.derek.mall.product.controller;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,14 +32,15 @@ public class CategoryController {
     private CategoryService categoryService;
 
     /**
-     * 列表
+     * 查出所有分类以及子分类，以树形结构组装起来
      */
-    @RequestMapping("/list")
+    @RequestMapping("/list/tree")
     // @RequiresPermissions("product:category:list")
-    public R list(@RequestParam Map<String, Object> params){
-        PageUtils page = categoryService.queryPage(params);
+    public R list(){
 
-        return R.ok().put("page", page);
+        List<CategoryEntity> entityList = categoryService.listWithTree();
+
+        return R.ok().put("page", entityList);
     }
 
 
